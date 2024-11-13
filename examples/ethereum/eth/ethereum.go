9ce9e417a5bb3dfff094d64ac74d681010e6063c
BiPhan4 <biphan374@gmail.com>
2024-11-13 12:50:56 -0500

    declare ethereum wrapper

---
+++ examples/ethereum/eth/ethereum.go
@@ -0,0 +1,17 @@
+package ethereum
+
+import (
+	"crypto/ecdsa"
+	"math/big"
+)
+
+// NOTE: This is a 'wrapper' object that works in conjunction with the 'EthereumChain' object
+// found in /chain/ethereum/ethereum_chain.go
+type Ethereum struct {
+	ChainID *big.Int
+	RPC     string
+	//EthAPI          EthAPI
+	//BeaconAPIClient *BeaconAPIClient	NOTE: Eureka used beacon for what?
+
+	Faucet *ecdsa.PrivateKey
+}

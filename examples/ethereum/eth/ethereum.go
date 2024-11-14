babe60ce28d0de3250f9442b9518c513720d7f45
BiPhan4 <biphan374@gmail.com>
2024-11-14 11:38:49 -0500

    add constructor for eth wrapper

--- examples/ethereum/eth/ethereum.go
+++ examples/ethereum/eth/ethereum.go
@@ -1,8 +1,11 @@
 package eth
 
 import (
+	"context"
 	"crypto/ecdsa"
 	"math/big"
+
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 // NOTE: This is a 'wrapper' object that works in conjunction with the 'EthereumChain' object
@@ -10,8 +13,31 @@ import (
 type Ethereum struct {
 	ChainID *big.Int
 	RPC     string
-	//EthAPI          EthAPI
+	EthAPI  EthAPI
 	//BeaconAPIClient *BeaconAPIClient	NOTE: Eureka used beacon for what?
 
 	Faucet *ecdsa.PrivateKey
+}
+
+func NewEthereum(ctx context.Context, rpc string, faucet *ecdsa.PrivateKey) (Ethereum, error) {
+	ethClient, err := ethclient.Dial(rpc)
+	if err != nil {
+		return Ethereum{}, err
+	}
+	chainID, err := ethClient.ChainID(ctx)
+	if err != nil {
+		return Ethereum{}, err
+	}
+
+	ethAPI, err := NewEthAPI(rpc)
+	if err != nil {
+		return Ethereum{}, err
+	}
+
+	return Ethereum{
+		ChainID: chainID,
+		RPC:     rpc,
+		EthAPI:  ethAPI,
+		Faucet:  faucet,
+	}, nil
 }

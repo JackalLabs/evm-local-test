93ee5d045635de413b25274244b160dfc6e8a0ba
BiPhan4 <biphan374@gmail.com>
2024-11-18 14:43:03 -0500

    basic dummy test to see if chains boot up - doesn't work

--- examples/ethereum/outpost_test.go
+++ examples/ethereum/outpost_test.go
@@ -1,4 +1,56 @@
 package main
 
-type OutpostSuite struct {
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/strangelove-ventures/interchaintest/v7/examples/ethereum/e2esuite"
+	"github.com/strangelove-ventures/interchaintest/v7/examples/ethereum/eth"
+	"github.com/stretchr/testify/suite"
+)
+
+type OutpostTestSuite struct {
+	e2esuite.TestSuite
+
+	// Whether to generate fixtures for the solidity tests
+	generateFixtures bool
+
+	// The private key of a test account
+	key *ecdsa.PrivateKey
+	// The private key of the faucet account of interchaintest
+	deployer *ecdsa.PrivateKey
+
+	contractAddresses eth.DeployedContracts
+}
+
+func (s *OutpostTestSuite) SetupSuite(ctx context.Context) {
+	s.TestSuite.SetupSuite(ctx)
+
+	eth, canined := s.ChainA, s.ChainB
+	fmt.Println(eth)
+	fmt.Println(canined)
+
+}
+
+func TestWithOutpostTestSuite(t *testing.T) {
+	suite.Run(t, new(OutpostTestSuite))
+}
+
+func (s *OutpostTestSuite) TestDummy() {
+	ctx := context.Background()
+	s.SetupSuite(ctx)
+
+	canined := s.ChainB
+	fmt.Println(canined)
+	time.Sleep(10 * time.Hour)
+
+	s.Require().True(s.Run("dummy", func() {
+
+		fmt.Println("made it here")
+		time.Sleep(10 * time.Hour)
+
+	}))
 }

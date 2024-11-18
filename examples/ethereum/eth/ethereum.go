3b847adbcba2ee743b12ec24961492b7c87b4de0
BiPhan4 <biphan374@gmail.com>
2024-11-18 15:39:59 -0500

    create and fund eth user

--- examples/ethereum/eth/ethereum.go
+++ examples/ethereum/eth/ethereum.go
@@ -3,9 +3,17 @@ package eth
 import (
 	"context"
 	"crypto/ecdsa"
+	"fmt"
 	"math/big"
+	"os"
+	"os/exec"
+	"strings"
 
+	"cosmossdk.io/math"
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/strangelove-ventures/interchaintest/v7/examples/ethereum/testvalues"
 )
 
 // NOTE: This is a 'wrapper' object that works in conjunction with the 'EthereumChain' object
@@ -40,4 +48,41 @@ func NewEthereum(ctx context.Context, rpc string, faucet *ecdsa.PrivateKey) (Eth
 		EthAPI:  ethAPI,
 		Faucet:  faucet,
 	}, nil
+}
+
+func (e Ethereum) CreateAndFundUser() (*ecdsa.PrivateKey, error) {
+	key, err := crypto.GenerateKey()
+	if err != nil {
+		return nil, err
+	}
+
+	address := crypto.PubkeyToAddress(key.PublicKey).Hex()
+	if err := e.FundUser(address, testvalues.StartingEthBalance); err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
+
+func (e Ethereum) FundUser(address string, amount math.Int) error {
+	return e.SendEth(e.Faucet, address, amount)
+}
+
+func (e Ethereum) SendEth(key *ecdsa.PrivateKey, toAddress string, amount math.Int) error {
+	cmd := exec.Command(
+		"cast",
+		"send",
+		toAddress,
+		"--value", amount.String(),
+		"--private-key", fmt.Sprintf("0x%s", ethcommon.Bytes2Hex(key.D.Bytes())),
+		"--rpc-url", e.RPC,
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to send eth with %s: %w", strings.Join(cmd.Args, " "), err)
+	}
+
+	return nil
 }

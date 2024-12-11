d7b59205e34ce8948973719b5e99872055388e22
BiPhan4 <biphan374@gmail.com>
2024-12-11 12:50:44 -0500

    utility for pulling mulberry image

---
+++ examples/ethereum/e2esuite/jackal_relay.go
@@ -0,0 +1,38 @@
+package e2esuite
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+// Utility for pulling and using an image of mulberry
+func PullImage(image string) error {
+	// Create a Docker client
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return fmt.Errorf("failed to create Docker client: %w", err)
+	}
+
+	// Context for the Docker API call
+	ctx := context.Background()
+
+	// Pull the Docker image
+	out, err := cli.ImagePull(ctx, image, types.ImagePullOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to pull image: %w", err)
+	}
+	defer out.Close()
+
+	// Print the output from the pull operation
+	if _, err := io.Copy(os.Stdout, out); err != nil {
+		return fmt.Errorf("failed to read pull output: %w", err)
+	}
+
+	fmt.Println("\nImage pulled successfully")
+	return nil
+}

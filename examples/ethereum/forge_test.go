5d1af3056b044f69f02ae0dcf4cc9d5e64123475
BiPhan4 <biphan374@gmail.com>
2025-01-02 19:03:04 -0500

    define sender and recipient gives gogoproto error

--- examples/ethereum/forge_test.go
+++ examples/ethereum/forge_test.go
@@ -3,9 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/strangelove-ventures/interchaintest/v7/examples/ethereum/eth"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func (s *OutpostTestSuite) SetupForgeSuite(ctx context.Context) {
@@ -31,6 +35,21 @@ func (s *OutpostTestSuite) SetupForgeSuite(ctx context.Context) {
 func (s *OutpostTestSuite) TestForge() {
 	ctx := context.Background()
 	s.SetupForgeSuite(ctx)
+
+	// Connect to Anvil RPC
+	rpcURL := "http://127.0.0.1:8545"
+	client, err := ethclient.Dial(rpcURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+	defer client.Close()
+
+	// Define accounts and their private keys
+	privateKeyA := "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+	fmt.Println(privateKeyA)
+	// Recipient B address
+	addressB := common.HexToAddress("0x70997970C51812dc3A010C7d01b50e0d17dc79C8")
+	fmt.Println(addressB)
 
 	s.Require().True(s.Run("forge", func() {
 		fmt.Println("made it to the end")

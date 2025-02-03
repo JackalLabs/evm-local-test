2b70a2f0117bb5cf7e98697afcc95ff4d88c5475
Anthony Chen <31290848+slightlyskepticalpotat@users.noreply.github.com>
2025-02-03 15:23:20 -0500

    update jackal contract

--- forge/src/Jackal.sol
+++ forge/src/Jackal.sol
@@ -4,14 +4,24 @@ pragma solidity ^0.8.0;
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 abstract contract Jackal {
-    event PostedFile(address sender, string merkle, uint64 size);
-    event Debug(string message); // Debug event
+    event PostedFile(address sender, string merkle, uint64 size, string note);
+    event BoughtStorage(address from, string for_address, uint64 duration_days, uint64 size_bytes, string referral);
 
-    function getPrice() public view virtual returns(int);
+    function getPrice() public view virtual returns (int256);
 
     mapping(address => mapping(address => bool)) public allowances;
 
-    function getAllowance(address from, address to) public view returns(bool) {
+    modifier validAddress() {
+        require(msg.sender != address(0), "Invalid sender address");
+        _;
+    }
+
+    modifier hasAllowance(address from) {
+        require(getAllowance(msg.sender, from), "No allowance set for contract");
+        _;
+    }
+
+    function getAllowance(address from, address to) public view returns (bool) {
         if (from == to) {
             return true;
         }
@@ -26,15 +36,13 @@ abstract contract Jackal {
         allowances[msg.sender][allow] = false;
     }
 
-    function getStoragePrice(uint64 filesize) public view returns (uint256) {
+    function getStoragePrice(uint64 filesize, uint256 months) public view returns (uint256) {
         uint256 price = uint256(getPrice());
-
         uint256 storagePrice = 15; // price at 8 decimal places
         uint256 multiplier = 2;
-        uint256 months = 200 * 12;
 
         uint256 fs = filesize;
-        if (fs  <= 1024 * 1024) {
+        if (fs <= 1024 * 1024) {
             fs = 1024 * 1024; // minimum file size of one MB for pricing
         }
 
@@ -50,13 +58,37 @@ abstract contract Jackal {
         return p;
     }
 
-    function postFileFrom(address from, string memory merkle, uint64 filesize) public payable{
-        require(msg.sender != address(0), "Invalid sender address");
-        require(getAllowance(msg.sender, from), "No allowance for this contract set");
-        emit PostedFile(from, merkle, filesize);
+    function postFileFrom(address from, string memory merkle, uint64 filesize, string memory note)
+        public
+        payable
+        validAddress
+        hasAllowance(from)
+    {
+        uint256 pE = getStoragePrice(filesize, 2400); // 12 * 200 months
+        require(msg.value >= pE, string.concat("Insufficient payment, need ", Strings.toString(pE), " wei"));
+        emit PostedFile(from, merkle, filesize, note);
     }
 
-    function postFile(string memory merkle, uint64 filesize) public payable{
-        postFileFrom(msg.sender, merkle, filesize);
+    function postFile(string memory merkle, uint64 filesize, string memory note) public payable {
+        postFileFrom(msg.sender, merkle, filesize, note);
+    }
+
+    function buyStorageFrom(
+        address from,
+        string memory for_address,
+        uint64 duration_days,
+        uint64 size_bytes,
+        string memory referral
+    ) public payable validAddress hasAllowance(from) {
+        uint256 pE = getStoragePrice(size_bytes, duration_days / 30); // months
+        require(msg.value >= pE, string.concat("Insufficient payment, need ", Strings.toString(pE), " wei"));
+        emit BoughtStorage(from, for_address, duration_days, size_bytes, referral);
+    }
+
+    function buyStorage(string memory for_address, uint64 duration_days, uint64 size_bytes, string memory referral)
+        public
+        payable
+    {
+        buyStorageFrom(msg.sender, for_address, duration_days, size_bytes, referral);
     }
 }
--- forge/src/JackalInterface.sol
+++ forge/src/JackalInterface.sol
@@ -2,6 +2,16 @@
 pragma solidity ^0.8.26;
 
 interface JackalInterface {
-    function postFile(string memory merkle, uint64 filesize) external payable;
-    function postFileFrom(address from, string memory merkle, uint64 filesize) external payable;
+    function postFile(string memory merkle, uint64 filesize, string memory note) external payable;
+    function postFileFrom(address from, string memory merkle, uint64 filesize, string memory note) external payable;
+    function buyStorage(string memory for_address, uint64 duration_days, uint64 size_bytes, string memory referral)
+        external
+        payable;
+    function buyStorageFrom(
+        address from,
+        string memory for_address,
+        uint64 duration_days,
+        uint64 size_bytes,
+        string memory referral
+    ) external payable;
 }
